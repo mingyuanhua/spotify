cc145ef938a884108e8be3ddda2a055032be8268
mingyuanhua <m.y.hua@hotmail.com>
2023-07-22 10:46:02 -0700

    Add EntryPoint to fragments

--- app/src/main/java/com/hmy/spotify/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/hmy/spotify/ui/favorite/FavoriteFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.hmy.spotify.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoriteFragment : Fragment() {
 
     override fun onCreateView(
