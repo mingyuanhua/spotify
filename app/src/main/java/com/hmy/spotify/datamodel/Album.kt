4a6e3d780821c8343e856642c364d8a1f69326b9
mingyuanhua <m.y.hua@hotmail.com>
2023-07-23 10:04:32 -0700

    Add Entity and PrimaryKey to Album data class

--- app/src/main/java/com/hmy/spotify/datamodel/Album.kt
+++ app/src/main/java/com/hmy/spotify/datamodel/Album.kt
@@ -1,8 +1,13 @@
 package com.hmy.spotify.datamodel
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
+@Entity
 data class Album(
+    @PrimaryKey
     val id: Int,
     @SerializedName("album")
     val name: String,
@@ -10,7 +15,7 @@ data class Album(
     val cover: String,
     val artists: String,
     val description: String
-) {
+) : Serializable {
 
     companion object {
         fun empty(): Album {
