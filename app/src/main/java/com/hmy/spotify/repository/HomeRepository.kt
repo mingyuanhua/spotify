15e5098c8850ccf1c51b4dc7ebf9f8cb18986e64
mingyuanhua <m.y.hua@hotmail.com>
2023-07-28 11:36:02 -0700

    Add HomeRepository

---
+++ app/src/main/java/com/hmy/spotify/repository/HomeRepository.kt
@@ -0,0 +1,16 @@
+package com.hmy.spotify.repository
+
+import com.hmy.spotify.datamodel.Section
+import com.hmy.spotify.network.NetworkApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class HomeRepository @Inject constructor(private val networkApi: NetworkApi) {
+
+    suspend fun getHomeSections(): List<Section> = withContext(Dispatchers.IO) {
+        delay(3000)
+        networkApi.getHomeFeed().execute().body()!!
+    }
+}
